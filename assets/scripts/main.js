128e1009a3e07a25693749d5752f72894abc45cd
Brent O'Neill <brentoneill@gmail.com>
2015-07-06 23:44:21 -0400

    Updated stylesheets, pages. Updated gulp file for build process

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  'use strict';
+
   // Quotes slider
   // $("#owl-carousel").owlCarousel({
   //
@@ -8,56 +10,5 @@ $(document).ready(function() {
   //     singleItem:true
   //
   // });
-  'use strict';
-
-  // Caching jQuery Variables for optimization
-  var $menuButton = $('.menu-button');
-  var $fullScreenMenu = $('.nav-fullscreen');
-  var $body = $('body');
-  var headerOffset = $('header').height();
-  var subscribeBarOffest = $('div.subscribe').offset().top;
-  var navItems = $('.nav-fullscreen ul').find('li');
-
-
-  // In case page loads below header, show menu button
-  // as orange by default
-  if ( $(window).scrollTop() > 60 + $('header').height()) {
-    $menuButton.addClass('orange');
-  }
-
-  // Handles opening and closing of menu
-  $body.on('click', '.menu-button-wrapper', function(e){
-    e.preventDefault();
-    // Toggles all classes for full screen menu
-    $menuButton.toggleClass('active');
-    $fullScreenMenu.toggleClass('active');
-    $body.toggleClass('no-scroll');
-    $('.page-content').toggleClass('blurred');
-
-    // ANIMATES NAV ITEMS - Delay set in CSS
-    for ( var i = 0 ; i < navItems.length ; i++ ) {
-      $(navItems[i]).toggleClass('animated');
-      $(navItems[i]).toggleClass('fadeIn');
-    }
-  });
-
-  // Changes color of menu button if scrolling past header
-  $(window).scroll(function (e) {
-    e.preventDefault();
-    var scroll = $(window).scrollTop();
-    console.log(subscribeBarOffest);
-    if ( scroll + 60 >=  headerOffset) {
-      $menuButton.addClass('orange');
-    }
-    else if ( scroll + 60 < headerOffset ) {
-      $menuButton.removeClass('orange');
-    }
-  });
-
-  // Prevents selection if double clicked
-  $body.on('dblClick', '.menu-button-wrapper', function(e){
-    e.preventDefault();
-  });
-
 
 });
