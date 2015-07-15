f97f4db70519d1155084766fd70d433f95023121
Brent O'Neill <brentoneill@gmail.com>
2015-07-14 20:23:13 -0400

    Added wow.js for animations on reveal using animate.css -- only implemented on home page, currently.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
 
   'use strict';
 
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       false,       // trigger animations on mobile devices (default is true)
+      live:         true       // act on asynchronously loaded content (default is true)
+    }
+  );
+
+
+  wow.init();
+
   // Quotes slider
   // $("#owl-carousel").owlCarousel({
   //
